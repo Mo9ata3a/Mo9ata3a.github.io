d1584e38c6f2d1b9157a9d0e5a37c57d5aae1b68
Mo9ata3a <terriblymekar@gmail.com>
2025-06-13 12:39:42 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Mo9ata3a.github.io/', // Remplacez par le nom de votre dépôt
+  base: '/', // Remplacez par le nom de votre dépôt
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
